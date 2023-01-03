18758d29a1219cd4d288a0afc231a6e1523c5dc2
szymcio4581 <lukaszszymura9@gmail.com>
2023-01-03 15:55:22 +0100

    poprawioned
    masz kubuś <3

--- main.cpp
+++ main.cpp
@@ -5,22 +5,19 @@
 using namespace std;
 
 int main() {
-  // Otwórz plik tekstowy "dane.txt".
+    cout << "Program czytajÄ…cy rzeczy z pliku\n";
   ifstream plik("dane.txt");
 
-  // SprawdŸ, czy plik zosta³ pomyœlnie otwarty.
   if (!plik.is_open()) {
-    cerr << "Nie uda³o siê otworzyæ pliku!" << endl;
+    cerr << "Nie udalo sie otworzyc pliku!" << endl;
     return 1;
   }
 
-  // WyprowadŸ dane z pliku tekstowego po jednej linii.
   string linia;
   while (getline(plik, linia)) {
     cout << linia << endl;
   }
 
-  // Zamykamy plik.
   plik.close();
 
   return 0;
